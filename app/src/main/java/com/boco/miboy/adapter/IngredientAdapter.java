29aaa92ff16e96583d3dd6b209606c9ae4382cef
Yurii Kotov <ykot@ciklum.com>
2016-11-27 12:34:32 +0200

    Buy dialog

--- app/src/main/java/com/boco/miboy/adapter/IngredientAdapter.java
+++ app/src/main/java/com/boco/miboy/adapter/IngredientAdapter.java
@@ -3,11 +3,13 @@ package com.boco.miboy.adapter;
 import android.content.Context;
 import android.support.annotation.ColorInt;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.boco.miboy.R;
@@ -17,6 +19,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
     private static final String TAG = IngredientAdapter.class.getSimpleName();
@@ -41,10 +44,13 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.Vi
 
         String item = " - " + ingredient.getKey();
         boolean available = ingredient.getValue() == 1;
+        int buyBtnVisibility = available ? View.GONE : View.VISIBLE;
         @ColorInt int color = available
                 ? ContextCompat.getColor(context, R.color.primary_text)
                 : ContextCompat.getColor(context, R.color.secondary_text);
 
+
+        holder.buy.setVisibility(buyBtnVisibility);
         holder.text.setText(item);
         holder.text.setTextColor(color);
     }
@@ -58,6 +64,19 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.Vi
     class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.text_view)
         TextView text;
+        @BindView(R.id.buy_btn)
+        Button buy;
+
+        @OnClick(R.id.buy_btn)
+        public void onBuyClick(View v) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.dialog_buy, null);
+            AlertDialog dialog = new AlertDialog.Builder(context)
+                    .setTitle("Buy product")
+                    .setView(view)
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        }
 
         ViewHolder(View itemView) {
             super(itemView);
